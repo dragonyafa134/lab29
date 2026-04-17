603a9b5be4d29f03bde7e32563b909dc54c4eada
dragonyafa134 <dasha.afanaseva.1206@mail.ru>
2026-04-17 23:07:13 +0300

    Step 2: TaskItem model and DTOs created

---
+++ TaskApi/Models/CreateTaskDto.cs
@@ -0,0 +1,7 @@
+namespace TaskAp1.Models;
+
+public class CreateTaskDto {
+    public string Title {get; set;} = string.Empty;
+    public string Description {get; set;} = string.Empty;
+    public string Priority {get; set;} = "Normal";
+}
